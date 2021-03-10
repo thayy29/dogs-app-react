9dc677e468545c4d448e48740f51a034467ba278
Thayana <thayana.cs@gmail.com>
2021-03-10 18:31:33 -0300

    feat/feed-layout-done

---
+++ src/Components/Feed/FeedPhotosItem.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { PHOTO_POST } from "../../api";
+import styles from "./FeedPhotosItem.module.css";
+
+const FeedPhotosItem = ({ photo }) => {
+  return (
+    <li className={styles.photo}>
+      <img src={photo.src} alt={photo.title} />
+      <span className={styles.visualizacao}>{photo.acessos}</span>
+    </li>
+  );
+};
+
+export default FeedPhotosItem;
