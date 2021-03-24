5963053e76c884c9c6d2168a6e83f5c9f5e986fd
Thayana <thayana.cs@gmail.com>
2021-03-24 15:49:52 -0300

    feat/statistics-1

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import React from "react";
-import Api from "./api/Api";
-import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Header from "./Components/Header";
-import Footer from "./Components/Footer";
-import Home from "./Components/Home";
-import Login from "./Components/Login/Login";
-import { UserStorage } from "./UserContext";
-import User from "./Components/User/User";
-import ProtectedRoute from "./Components/Helper/ProtectedRoute";
-import Photo from "./Components/Photo/Photo";
-import UserProfile from "./Components/User/UserProfile";
-import NotFound from "./Components/NotFound";
+import React from 'react';
+import Api from './api/Api';
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+import Home from './Components/Home';
+import Login from './Components/Login/Login';
+import { UserStorage } from './UserContext';
+import User from './Components/User/User';
+import ProtectedRoute from './Components/Helper/ProtectedRoute';
+import Photo from './Components/Photo/Photo';
+import UserProfile from './Components/User/UserProfile';
+import NotFound from './Components/NotFound';
 
 const App = () => {
   return (
-    <div>
+    <div className="App">
       <BrowserRouter>
         <UserStorage>
           <Header />
--- src/Components/User/UserStats.js
+++ src/Components/User/UserStats.js
@@ -1,12 +1,33 @@
-import React from "react";
-import Head from "../Helper/Head";
+import React from 'react';
+import Head from '../Helper/Head';
+import useFetch from '../../Hooks/useFetch';
+import Loading from '../Helper/Loading';
+import Error from '../Helper/Error';
+import { STATS_GET } from '../../api';
+import UserStatsGraphs from './UserStatsGraphs';
 
 const UserStats = () => {
-  return (
-    <div>
-      <Head title="Estatísticas" />
-    </div>
-  );
+  const { data, error, loading, request } = useFetch();
+
+  React.useEffect(() => {
+    async function getData() {
+      const { url, options } = STATS_GET();
+      await request(url, options);
+    }
+    getData();
+  }, [request]);
+
+  if (loading) return <Loading />;
+  if (error) return <Error error={error} />;
+  if (data)
+    return (
+      <div>
+        <Head title="Estatísticas" />
+
+        <UserStatsGraphs data={data} />
+      </div>
+    );
+  else return null;
 };
 
 export default UserStats;
